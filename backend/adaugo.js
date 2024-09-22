e891116e8948583847564babc78f0582e67a6d75
Abuchi Robert <abuchiahamefula2019@gmail.com>
2024-09-22 16:40:47 +0100

    2nd

---
+++ backend/adaugo.js
@@ -0,0 +1,15 @@
+
+
+const Yourname = "Adaugo" // You cannot change the data 
+let age = 15
+var address = "101 Ukaegbu Road"
+
+
+console.log("My name is ", Yourname, "I am", age, "years old")
+console.log("Yourname")
+console.log("age")
+console.log("address")
+
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
